cbc5dda8c5903154828536caf6e7a02086538def
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-04-24 17:28:09 +0100

    read dir using filesystem class

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,27 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique velit ut lorem commodo viverra. Quisque tellus lorem, egestas sit amet urna a, posuere tincidunt lacus. Maecenas hendrerit tellus sed ullamcorper laoreet. Praesent vitae mauris at erat efficitur vehicula nec eget leo. Vivamus porttitor, lorem vel porta lobortis, quam tellus elementum leo, vel faucibus felis ligula sed enim. Maecenas cursus tellus a ullamcorper laoreet. Aenean leo diam, convallis faucibus venenatis ac, tincidunt sed erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla lobortis finibus molestie. Nulla eu augue ut lectus luctus gravida quis sed lectus. Aenean accumsan arcu at suscipit rhoncus. Aenean lobortis ligula ligula, vel luctus urna ultricies nec.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique velit ut lorem commodo viverra. Quisque tellus lorem, egestas sit amet urna a, posuere tincidunt lacus. Maecenas hendrerit tellus sed ullamcorper laoreet. Praesent vitae mauris at erat efficitur vehicula nec eget leo. Vivamus porttitor, lorem vel porta lobortis, quam tellus elementum leo, vel faucibus felis ligula sed enim. Maecenas cursus tellus a ullamcorper laoreet. Aenean leo diam, convallis faucibus venenatis ac, tincidunt sed erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla lobortis finibus molestie. Nulla eu augue ut lectus luctus gravida quis sed lectus. Aenean accumsan arcu at suscipit rhoncus. Aenean lobortis ligula ligula, vel luctus urna ultricies nec.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique velit ut lorem commodo viverra. Quisque tellus lorem, egestas sit amet urna a, posuere tincidunt lacus. Maecenas hendrerit tellus sed ullamcorper laoreet. Praesent vitae mauris at erat efficitur vehicula nec eget leo. Vivamus porttitor, lorem vel porta lobortis, quam tellus elementum leo, vel faucibus felis ligula sed enim. Maecenas cursus tellus a ullamcorper laoreet. Aenean leo diam, convallis faucibus venenatis ac, tincidunt sed erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla lobortis finibus molestie. Nulla eu augue ut lectus luctus gravida quis sed lectus. Aenean accumsan arcu at suscipit rhoncus. Aenean lobortis ligula ligula, vel luctus urna ultricies nec.
-        </p>
-    </article>
+    <?php foreach($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>
