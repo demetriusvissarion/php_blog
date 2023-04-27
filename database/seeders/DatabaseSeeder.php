525e64341b9aa5b4fb4f39859f1e12982d800e6a
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-04-27 10:49:43 +0100

    database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +15,60 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user = User::factory(10)->create();
+
+        // dd($user);
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+        // foreach($users as $user){
+        Post::create([
+            'user_id' => $user[0]->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => 'Excerpt for my post',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique velitut lorem commodo viverra. Quisque tellus lorem, egestas sit amet urna a, tincidunt lacus. Maecenas hendrerit tellus sed ullamcorper laoreet. Praesent vitae mauris at erat efficitur vehicula nec eget leo. Vivamus porttitor, lorem vel porta lobortis, quam tellus elementum leo, vel faucibus felis ligula sed enim. Maecenas cursus tellus a ullamcorper laoreet. Aenean leo diam, convallis faucibus venenatis ac, tincidunt sed erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla lobortis finibus molestie. Nulla eu augue ut lectus luctus gravida quis sed lectus. Aenean accumsan arcu at suscipit rhoncus. Aenean lobortis ligula ligula, vel luctus urna ultricies nec.</p>',
+        ]);
+
+
+        //     $title = 'My work post'.rand();
+        //     Post::create([
+        //         'user_id' => $user->id,
+        //         'category_id' => $work->id,
+        //         'title' => $title,
+        //         'slug' => str_slug($title),
+        //         'excerpt' => 'Excerpt for my post',
+        //         'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique velitut lorem commodo viverra. Quisque tellus lorem, egestas sit amet urna a, tincidunt lacus. Maecenas hendrerit tellus sed ullamcorper laoreet. Praesent vitae mauris at erat efficitur vehicula nec eget leo. Vivamus porttitor, lorem vel porta lobortis, quam tellus elementum leo, vel faucibus felis ligula sed enim. Maecenas cursus tellus a ullamcorper laoreet. Aenean leo diam, convallis faucibus venenatis ac, tincidunt sed erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla lobortis finibus molestie. Nulla eu augue ut lectus luctus gravida quis sed lectus. Aenean accumsan arcu at suscipit rhoncus. Aenean lobortis ligula ligula, vel luctus urna ultricies nec.</p>',
+        //     ]);
+        // }
+
+
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-work-post',
+        //     'excerpt' => 'Excerpt for my post',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique velitut lorem commodo viverra. Quisque tellus lorem, egestas sit amet urna a, tincidunt lacus. Maecenas hendrerit tellus sed ullamcorper laoreet. Praesent vitae mauris at erat efficitur vehicula nec eget leo. Vivamus porttitor, lorem vel porta lobortis, quam tellus elementum leo, vel faucibus felis ligula sed enim. Maecenas cursus tellus a ullamcorper laoreet. Aenean leo diam, convallis faucibus venenatis ac, tincidunt sed erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla lobortis finibus molestie. Nulla eu augue ut lectus luctus gravida quis sed lectus. Aenean accumsan arcu at suscipit rhoncus. Aenean lobortis ligula ligula, vel luctus urna ultricies nec.</p>',
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
