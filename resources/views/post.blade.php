5de31e760f1a7925da6ba4b8ca10f6d056e270ee
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-03 13:24:24 +0100

    refactoring and clean-up

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,12 @@
 <x-layout>
     <section class="px-6 py-8">
-
         <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
                 <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
                     <img src="/images/illustration-1.png" alt="" class="rounded-xl">
-
                     <p class="mt-4 block text-gray-400 text-xs">
-                        Published <time>1 day ago</time>
+                        Published <time>{{ $post->created_at->diffForHumans() }}</time>
                     </p>
-
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar">
                         <div class="ml-3 text-left">
@@ -17,7 +14,6 @@
                         </div>
                     </div>
                 </div>
-
                 <div class="col-span-8">
                     <div class="hidden lg:flex justify-between mb-6">
                         <a href="/"
@@ -31,29 +27,22 @@
                                     </path>
                                 </g>
                             </svg>
-
                             Back to Posts
                         </a>
-
                         <div class="space-x-2">
                             <x-category-button :category="$post->category" />
                         </div>
                     </div>
-
                     <h1 class="font-bold text-3xl lg:text-4xl mb-10">
                         {{ $post->title }}
                     </h1>
 
-                    <div class="space-y-4 lg:text-lg leading-loose">
-                        {{ $post->body }}
-                    </div>
+                    <div class="space-y-4 lg:text-lg leading-loose">{!! $post->body !!}</div>
                 </div>
             </article>
         </main>
-
     </section>
 </x-layout>
 
 
 {{-- 'Date:  ' . DateTime::createFromFormat('U', $post->date)->format('d/m/Y') --}}
-
