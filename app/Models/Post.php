eadbad16d08adc8724a3774192bcb26d2e2ccc40
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-05 18:08:06 +0100

    merge categories and search queries

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,41 +1,30 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
     use HasFactory;
-
     protected $guarded = [];
-
     protected $with = ['category', 'author'];
-
-    public function scopeFilter($query, array $filters)  // will be called using: Post::newQuery()->filter();
+    public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, fn ($query, $search) =>
-        $query
-            ->where('title', 'like', '%' . $search . '%')
-            ->orWhere('body', 'like', '%' . $search . '%'));
-
-        $query->when(
-            $filters['category'] ?? false,
-            fn ($query, $category) =>
-            $query->whereHas(
-                'category',
-                fn ($query) => $query->where('slug', $category)
-            )
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'))
         );
 
-        $query->when(
-            $filters['author'] ?? false,
-            fn ($query, $author) =>
-            $query->whereHas(
-                'author',
-                fn ($query) => $query->where('username', $author)
-            )
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn ($query) =>
+                $query->where('slug', $category)
+        )
+        );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query->whereHas('author', fn ($query) =>
+                $query->where('username', $author)
+        )
         );
     }
 
@@ -43,12 +32,6 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -10,8 +10,8 @@
             <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
                     <a href="/categories/{{ $post->category->slug }}"
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{ $post->category->name }}</a>
+                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+                       style="font-size: 10px">{{ $post->category->name }}</a>
                 </div>
                 <div class="mt-4">
                     <h1 class="text-3xl">
@@ -24,24 +24,23 @@
                     </span>
                 </div>
             </header>
-
             <div class="text-sm mt-2 space-y-4">
                 {!! $post->excerpt !!}
             </div>
-
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">
-                            <a href="/?author={{ $post->author->username }}" >{{ $post->author->name }}</a>
+                            <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
                         </h5>
                     </div>
                 </div>
+
                 <div class="hidden lg:block">
                     <a href="/posts/{{ $post->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
-                        More</a>
+                       class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                    >Read More</a>
                 </div>
             </footer>
         </div>
