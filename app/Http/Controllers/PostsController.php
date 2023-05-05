2d014fa8e09c6bb652a067c362abad81dffb2a97
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-05 13:18:20 +0100

    extract a category dropdown

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,23 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts', [
+        return view('posts.index', [
             'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory' => Category::where('slug', request('category'))->first(),
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('post.index', [
             'post' => $post,
         ]);
     }
