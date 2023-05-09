8a5106689a784b38dd3333428311e60dd0f14a62
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-09 15:41:23 +0100

    set JS timer on flash message

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -57,9 +57,5 @@
         </footer>
     </section>
 
-    @if (session()->has('success'))
-        <div class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">
-            <p>{{ session('success') }}</p>
-        </div>
-    @endif
+    <x-flash/>
 </body>
