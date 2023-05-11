93d5044120761576d7d3fa302316c7db0c61de6f
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-11 14:23:40 +0100

    refactoring

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+//        dd('string');
+        return view('sessions.create');
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+//        dd(auth()->attempt($attributes));
+        if (! auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
+
+        }
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Welcome Back!');
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
