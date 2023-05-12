451fd551673fc0e34971c5b32db5f16666083ef3
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-12 11:08:33 +0100

    refactored and made comments section dynamic

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author() // Laravel will assume here the foreign key is 'author_id' but it's not so we overwrite it to 'user-id'
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
