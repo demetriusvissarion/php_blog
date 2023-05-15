1c1fa3cfbdb62628859782cd5e5a9dea3817bc89
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-15 17:27:20 +0100

    limit access to only admins

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,23 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Contracts\View\View;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index(): View
     {
 //        dd('test');
         return view('posts.index', [
             'posts' => Post::latest()->filter(
-                        request(['search', 'category', 'author'])
-                    )->paginate(18)->withQueryString()
+                request(['search', 'category', 'author'])
+            )->paginate(18)->withQueryString()
         ]);
     }
 
-    public function show(Post $post)
+    public function show(Post $post): View
     {
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function create(): View
+    {
+        return view('posts.create');
     }
 }
