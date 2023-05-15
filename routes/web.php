c4239b905e8b329faba724c8d99c0d3a876ab1d1
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-15 13:18:13 +0100

    added mailchimp api

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('5372770900', [
+        'email_address' => 'test.test@gmail.com',
+        'status' => 'subscribed',
+    ]);
+
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
