68d807ea43125d3bb1e725a1be6b470ab17f1ab4
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-16 16:18:07 +0100

    moved logout into dropdown button

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,75 +23,90 @@
 </style>
 
 <body style="font-family: Open Sans, sans-serif">
-    <section class="px-6 py-8">
-        <nav class="md:flex md:justify-between md:items-center">
-            <div>
-                <a href="/">
-                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
-                </a>
-            </div>
+<section class="px-6 py-8">
+    <nav class="md:flex md:justify-between md:items-center">
+        <div>
+            <a href="/">
+                <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+            </a>
+        </div>
 
-            <div class="mt-8 md:mt-0 flex items-center">
-                @auth
-                    <span class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</span>
+        <div class="mt-8 md:mt-0 flex items-center">
+            @auth
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <button class="text-xs font-bold uppercase">Welcome, {{ auth()->user()->name }}!</button>
+                    </x-slot>
 
-                    <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
+                    <x-dropdown-item href="/admin/dashboard">Dashboard</x-dropdown-item>
+                    <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New
+                        Post
+                    </x-dropdown-item>
+                    <x-dropdown-item
+                        href="#"
+                        x-data="{}"
+                        @click.prevent="document.querySelector('#logout-form').submit()">Log Out
+                    </x-dropdown-item>
+
+                    <form id="logout-form" method="POST" action="/logout" class="hidden">
                         @csrf
-
-                        <button type="submit">Log Out</button>
                     </form>
-                @else
-                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
-                @endauth
+                </x-dropdown>
 
-                <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
-                    Subscribe for Updates
-                </a>
-            </div>
-        </nav>
+            @else
+                <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+            @endauth
 
-        {{ $slot }}
+            <a href="#newsletter"
+               class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                Subscribe for Updates
+            </a>
+        </div>
+    </nav>
 
-        <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
-            <h5 class="text-3xl">Stay in touch with the latest posts</h5>
-            <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
+    {{ $slot }}
 
-            <div class="mt-10">
-                <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
+    <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+        <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+        <h5 class="text-3xl">Stay in touch with the latest posts</h5>
+        <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
 
-                    <form method="POST" action="/newsletter" class="lg:flex text-sm">
-                        @csrf
+        <div class="mt-10">
+            <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
 
-                        <div class="lg:py-3 lg:px-5 flex items-center">
-                            <label for="email" class="hidden lg:inline-block">
-                                <img src="/images/mailbox-icon.svg" alt="mailbox letter">
-                            </label>
+                <form method="POST" action="/newsletter" class="lg:flex text-sm">
+                    @csrf
 
-                            <div>
-                                <input id="email"
-                                       name="email"
-                                       type="text"
-                                       placeholder="Your email address"
-                                       class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                    <div class="lg:py-3 lg:px-5 flex items-center">
+                        <label for="email" class="hidden lg:inline-block">
+                            <img src="/images/mailbox-icon.svg" alt="mailbox letter">
+                        </label>
 
-                                @error('email')
-                                    <span class="text-xs text-red-500">{{ $message }}</span>
-                                @enderror
-                            </div>
+                        <div>
+                            <input id="email"
+                                   name="email"
+                                   type="text"
+                                   placeholder="Your email address"
+                                   class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+
+                            @error('email')
+                            <span class="text-xs text-red-500">{{ $message }}</span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <button type="submit"
-                                class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
-                        >
-                            Subscribe
-                        </button>
-                    </form>
-                </div>
+                    <button type="submit"
+                            class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
+                    >
+                        Subscribe
+                    </button>
+                </form>
             </div>
-        </footer>
-    </section>
+        </div>
+    </footer>
+</section>
 
-    <x-flash />
+<x-flash/>
 </body>
