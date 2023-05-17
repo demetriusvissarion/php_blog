98a6b47d660cc78d317401cd560bb507bc2786d7
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-05-17 12:02:19 +0100

    grouped and stored validation logic

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -4,24 +4,24 @@
             @csrf
             @method('PATCH')
 
-            <x-form.input name="title" :value="old('title', $post->title)"/>
-            <x-form.input name="slug" :value="old('slug', $post->slug)"/>
+            <x-form.input name="title" :value="old('title', $post->title)" required/>
+            <x-form.input name="slug" :value="old('slug', $post->slug)" required/>
 
             <div class="flex mt-6">
                 <div class="flex-1">
                     <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)"/>
                 </div>
 
-                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="existing thumbnail" class="rounded-xl ml-6"
-                     width="100">
+                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl ml-6" width="100">
             </div>
 
-            <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
-            <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
+            <x-form.textarea name="excerpt" required>{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
+            <x-form.textarea name="body" required>{{ old('body', $post->body) }}</x-form.textarea>
 
             <x-form.field>
                 <x-form.label name="category"/>
-                <select name="category_id" id="category_id">
+
+                <select name="category_id" id="category_id" required>
                     @foreach (\App\Models\Category::all() as $category)
                         <option
                             value="{{ $category->id }}"
--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -1,4 +1,5 @@
 @props(['post'])
+
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5 lg:flex">
@@ -8,9 +9,7 @@
         <div class="flex-1 flex flex-col justify-between">
             <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
-                    <a href="/categories/{{ $post->category->slug }}"
-                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                       style="font-size: 10px">{{ $post->category->name }}</a>
+                    <x-category-button :category="$post->category"/>
                 </div>
                 <div class="mt-4">
                     <h1 class="text-3xl">
